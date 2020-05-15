9419785b736e71ec046c4f43f983f2f656a38276
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-05-15 16:36:16 -0300

    correção endpoint create session

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -11,9 +11,9 @@ class SessionController {
     const { cpf, password } = req.body;
 
     if (!cpf || !password || !this.utils.validateCpf(cpf) || cpf !== '76621002009' || password !== '123456')
-      return res.status(400).json({ success: false, code: 'invalid_data', message: 'CPF ou senha inválidos' });
+      return res.json({ success: false, code: 'invalid_data', message: 'CPF ou senha inválidos' });
 
-    return res.status(200).json({
+    return res.json({
       success: true,
       user: { nome: 'Jhonny Cooper', foto: 'https://blog.hotmart.com/blog/2019/01/BLOG_Perfil-empreendedor.png' },
       token: '5gbahb22ska52rhkiuta1096vg3ht7vj34k2m4234234'
@@ -21,7 +21,7 @@ class SessionController {
   }
 
   read(req, res) {
-    return res.status(200).json({
+    return res.json({
       success: true,
       message: 'Usuário autenticado'
     });
