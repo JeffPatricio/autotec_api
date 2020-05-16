dd278e2c312a1c1ae3f54f5653288afb4fc657d7
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-05-16 00:50:21 -0300

    correção em endpoint de session

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -1,9 +1,9 @@
 class Auth {
   validate(req, res, next) {
     const authHeader = req.headers.authorization;
-    if (!authHeader) return res.status(401).json({ success: false, code: 'invalid_token', message: 'O Token não foi fornecido ou é inválido.', auth: false });
+    if (!authHeader) return res.json({ success: false, code: 'invalid_token', message: 'O Token não foi fornecido ou é inválido.', auth: false });
     const token = authHeader.split(' ')[1];
-    if (token !== '5gbahb22ska52rhkiuta1096vg3ht7vj34k2m4234234') res.status(401).json({ success: false, code: 'invalid_token', message: 'O Token não foi fornecido ou é inválido.', auth: false });
+    if (token !== '5gbahb22ska52rhkiuta1096vg3ht7vj34k2m4234234') res.json({ success: false, code: 'invalid_token', message: 'O Token não foi fornecido ou é inválido.', auth: false });
     return next();
   }
 }
